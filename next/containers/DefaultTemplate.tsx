80dcaae5efd4acf69b7a863a054b8a15b0dde1ef
Drew Hutton <drew.business2308@gmail.com>
2020-01-10 10:54:36 +1030

    feat: cleanup types & add matrix example

--- next/containers/DefaultTemplate.tsx
+++ next/containers/DefaultTemplate.tsx
@@ -5,17 +5,33 @@ import { useQuery } from '@apollo/react-hooks';
 
 import Layout from './Layout';
 
+interface testMatrixItem {
+  testField1: string;
+  testField2: string;
+}
+interface Entry {
+  title: string;
+  subtitle: string;
+  testMatrix: testMatrixItem[];
+}
+
+interface DefaultPageTemplateQuery {
+  entries: Entry[];
+}
+
 const DefaultPageTemplate = ({ slug }: { slug: string | string[] }) => {
-  const { loading, error, data } = useQuery(query, {
+  const { loading, error, data } = useQuery<DefaultPageTemplateQuery>(query, {
     variables: { slug },
   });
 
   if (error) return <div>Error loading page</div>;
   if (loading) return <div>Loading</div>;
 
-  const {
-    entries: [{ title, subtitle }],
-  }: { entries: { title: string; subtitle: string }[] } = data;
+  const { entries } = data;
+
+  if (entries.length === 0) return <div>No Entries</div>;
+
+  const [{ title, subtitle, testMatrix }] = entries; // This kind of deconstruction only works when there is 1 item in an array be careful with it
 
   return (
     <Layout title={title}>
@@ -26,17 +42,32 @@ const DefaultPageTemplate = ({ slug }: { slug: string | string[] }) => {
       <div className='hero'>
         <h1 className='title'>{title}</h1>
         <p className='description'>{subtitle}</p>
+        {testMatrix.map((testMatrixItem: testMatrixItem, index) => (
+          <p key={`testMatrixItem.testField1-${index}`} className='description'>
+            {testMatrixItem.testField1} | {testMatrixItem.testField2}
+          </p>
+        ))}
       </div>
     </Layout>
   );
 };
 
 export const query = gql`
-  query HomePage($slug: [String]) {
+  query DefualtTemplate($slug: [String]) {
     entries(section: "Pages", slug: $slug) {
       ... on pages_pages_Entry {
         title
         subtitle
+        testMatrix {
+          ... on testMatrix_test1_BlockType {
+            testField1
+            testField2
+          }
+          ... on testMatrix_test2_BlockType {
+            testField1
+            testField2
+          }
+        }
       }
     }
   }
